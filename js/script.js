ded0062fb8b2b4f492066988bdc4ece682484b9c
koka-patsatsia <koka.patsatsia@gmail.com>
2025-06-14 13:52:56 +0400

    lesson1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-let x=50
+let x = 100;
+if(x<50){
+    console.log("ნაკლებია 50-ზე");
+} else if (x>20){
+    console.log("მეტია 20-ზე");
+} else {
+    console.log("შეცდომა");
+}
+    
+
+let userName = "Mariam";
+
+if(userName == "Mariam"){
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+switch(userName){
+    case "Mariam":
+        console.log("TRUE");
+        break;
+    default:
+        console.log("FALSE");
+}
